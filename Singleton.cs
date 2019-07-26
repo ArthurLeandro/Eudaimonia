bffd328864b46aea91019b5999acb0fa35734a74
Arthur Leandro <46695986+ArthurLeandro@users.noreply.github.com>
2019-07-26 11:21:09 -0300

    Update Singleton.cs
    Singleton Base Class

--- Singleton.cs
+++ Singleton.cs
@@ -1,1 +1,29 @@
+using UnityEngine;
+using System;
 
+public class Singleton<T>: MonoBehaviour{
+  private static bool quitting = false;
+  private static T instance;
+  public static T Instance{get{Init(); return instance;}
+  
+  
+  private void Init(){
+    if(instance==null || instance.Equals(null)){
+      var gameObject == new GameObject("Singleton Manager");
+      gameObject.hideFlags = HideFlags.HideAndDontSave;
+      instance = gameObject.AddComponent<Singleton>();
+      DontDestroyOnLoad(gameObject);
+    }
+    
+    private static bool OnApplicationQuit(){
+      quitting = true;
+    }
+    private void OnDestroy(){
+      if(!quitting){
+        instance = null;
+        Init();
+      }
+      
+    }
+  }
+}
